b87f32c25ee22daa7307d299ce7eab0e3ec56df3
Rius-K03 <kadrianrius2@gmail.com>
2026-07-15 17:34:34 -0400

    fix firebase database url

--- src/firebase.js
+++ src/firebase.js
@@ -13,7 +13,7 @@ import { getDatabase } from 'firebase/database';
 const firebaseConfig = {
   apiKey:            "PASTE_YOUR_API_KEY_HERE",
   authDomain:        "PASTE_YOUR_AUTH_DOMAIN_HERE",
-  databaseURL:       "PASTE_YOUR_DATABASE_URL_HERE",
+  databaseURL:       "https://cmpnd-league-default-rtdb.firebaseio.com",
   projectId:         "PASTE_YOUR_PROJECT_ID_HERE",
   storageBucket:     "PASTE_YOUR_STORAGE_BUCKET_HERE",
   messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID_HERE",
